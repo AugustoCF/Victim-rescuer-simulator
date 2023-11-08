c2e0eb8246e9a7c044792c14e41ec3b85caa2fcb
Augusto Freitas <gutocfreitas@gmail.com>
2023-11-08 14:24:32 -0300

    Reunir dados de todos as vitimas

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
 
 from aux_file import priority_directions
 
@@ -13,6 +16,8 @@ def main(data_folder_name):
     # Set the path to config files and data files for the environment
     current_folder = os.path.abspath(os.getcwd())
     data_folder = os.path.abspath(os.path.join(current_folder, data_folder_name))
+
+    all_victims = {}
 
     # Instantiate the environment
     env = Env(data_folder)
@@ -38,6 +43,50 @@ def main(data_folder_name):
     # Run the environment simulator
     env.run()
 
+    #print(exp1.NAME)
+    #for id, data in exp1.victims.items():
+    #        print(f"Id: {id}, Pos: {data.pos}, pulse: {data.pulse}, resp: {data.resp}, qPA: {data.qPA}")
+    
+    for id, data in exp1.victims.items():
+        all_victims[id] = data
+
+    for id, data in exp2.victims.items():
+        all_victims[id] = data
+
+    for id, data in exp3.victims.items():
+        all_victims[id] = data
+
+    for id, data in exp4.victims.items():
+        all_victims[id] = data
+    
+    for id, data in all_victims.items():
+        print(f"id: {id}, posicao x: {data.pos[0]} y: {data.pos[1]}")
+    
+
+def classify_victims (self) -> None:
+    pulse = ctrl.Antecedent(np.arange(0, 201, 1), 'Pulse')
+    qPA = ctrl.Antecedent(np.arange(-10, 11, 1), 'qPA')
+    resp = ctrl.Antecedent(np.arange(0, 23, 1), 'Resp')
+    classification = ctrl.Consequent(np.arange(1, 5, 1), 'Classification')
+
+    pulse['Low'] = fuzz.trimf(pulse.universe, [0, 1, 80])
+    pulse['Mid'] = fuzz.trimf(pulse.universe, [60, 80, 130])
+    pulse['High'] = fuzz.trimf(pulse.universe, [110, 200, 200])
+
+    resp['Low'] = fuzz.trimf(pulse.universe, [0, 0, 12])
+    resp['Mid'] = fuzz.trimf(pulse.universe, [10, 15, 21])
+    resp['High'] = fuzz.trimf(pulse.universe, [20, 22, 22])
+
+    qPA['Min-'] = fuzz.trimf(pulse.universe, [-10, -10, -2])
+    qPA['Min+'] = fuzz.trimf(pulse.universe, [2, 10, 10])
+    qPA['High'] = fuzz.trimf(pulse.universe, [-3, 0, 3])
+
+    classification['Crítico'] = fuzz.trimf(classification.universe, [1, 1, 2])
+    classification['Instável'] = fuzz.trimf(classification.universe, [1, 2, 3])
+    classification['Pot Estável'] = fuzz.trimf(classification.universe, [2, 3, 4])
+    classification['Estável'] = fuzz.trimf(classification.universe, [3, 4, 4])
+    
+    
 
 if __name__ == '__main__':
     """ To get data from a different folder than the default called data
